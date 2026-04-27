a22472e63e9d04a4472d34c4672694d491d20b9b
Isaac Kellogg <keldtdev@gmail.com>
2026-04-27 12:51:00 -0400

    Show dates on gallery tiles and image modal
    Also set a basic limit on the height of descriptions on gallery tiles. Descriptions that are incredibly long
    will now scroll, so they don't overshoot the bottom of the image itself.

--- src/components/galleryTile.jsx
+++ src/components/galleryTile.jsx
@@ -1,6 +1,7 @@
 import { thumbHashToDataURL } from 'thumbhash';
 import './galleryTile.css';
 
+
 export default function GalleryTile({ photoMeta, idx, onClickContent, photosPrefix}) {
     return (
         <>
@@ -9,17 +10,18 @@ export default function GalleryTile({ photoMeta, idx, onClickContent, photosPref
         {photoMeta.title && photoMeta.title.length > 0 && <h3 className="gallery-tile-title">{photoMeta.title}</h3>}
         <div className="gallery-tile-content" onClick={onClickContent}>
             <div className="gallery-tile-description">
-            <p>{photoMeta.description}</p>
+                <p>{photoMeta.description}</p>
+                <p>{new Date(photoMeta.dateTime.year, photoMeta.dateTime.month - 1, photoMeta.dateTime.day, photoMeta.dateTime.hour, photoMeta.dateTime.minute, photoMeta.dateTime.second).toLocaleDateString('en-US', {month: 'long', day: 'numeric', year: 'numeric'})}</p>
             </div>
             <div className="gallery-tile-image-container">
-            <img 
-                className="gallery-tile-image"
-                srcSet={`
-                ${photosPrefix}${photoMeta.derivatives[1].filename},
-                ${thumbHashToDataURL(Uint8Array.from(atob(photoMeta.thumbhash), c => c.charCodeAt(0)))}
-                `}
-                alt={photoMeta.title}
-            />
+                <img 
+                    className="gallery-tile-image"
+                    srcSet={`
+                    ${photosPrefix}${photoMeta.derivatives[1].filename},
+                    ${thumbHashToDataURL(Uint8Array.from(atob(photoMeta.thumbhash), c => c.charCodeAt(0)))}
+                    `}
+                    alt={photoMeta.title}
+                />
             </div>
         </div>
         <div className="gallery-tile-tags">
